3d8c854885af3cd6eab8472f2a0311a31f83510a
bewr <benjamin.werner@mfn.berlin>
2020-11-01 23:40:20 +0100

    add arm build script

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
 	rules: {
 		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
 		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-		'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+		'no-unused-vars': process.env.NODE_ENV === 'production' ? 'warn' : 'warn',
 		'vue/no-unused-components': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
 		'vue/no-use-v-if-with-v-for': 'off',
 	},
